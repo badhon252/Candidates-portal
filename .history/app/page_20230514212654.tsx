5785d7c3f4799eb86cc6344ce34e1138cbe672d4
khalid <dev.khalidhossain@gmail.com>
2023-05-14 21:28:05 +0600

    lint-staged added

--- .history/app/page_20230514212654.tsx
+++ .history/app/page_20230514212654.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-8xl font-extrabold">Hello World!</h1>
-      <h2>This is my Movie Voult website</h2>
     </main>
   );
 }
