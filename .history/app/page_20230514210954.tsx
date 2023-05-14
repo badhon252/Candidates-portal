86079601f0396676adb8ee2c6acc2245122e9078
khalid <dev.khalidhossain@gmail.com>
2023-05-14 21:11:00 +0600

    test

--- .history/app/page_20230514210954.tsx
+++ .history/app/page_20230514210954.tsx
@@ -5,3 +5,11 @@ export default function Home() {
     </main>
   );
 }
+
+var helloworld = () => {
+  return =()=>(
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-8xl font-extrabold">Hello World!</h1>
+    </main>
+  );
+};
